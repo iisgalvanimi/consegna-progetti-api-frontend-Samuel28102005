f56099e646b9e490b841bb910224c5086579d23f
Samuel28102005 <labarba.samuel@iisgalvanimi.edu.it>
2024-11-12 12:51:18 +0000

    manca http post

--- angular-routing/src/app/app.module.ts
+++ angular-routing/src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnimalsComponent } from './animals/animals.component';
 import { FruitsComponent } from './fruits/fruits.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { GenericComponent } from './generic/generic.component';
+import { FooComponent } from './foo/foo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AnimalsComponent,
     FruitsComponent,
-    GenericComponent
+    GenericComponent,
+    FooComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
